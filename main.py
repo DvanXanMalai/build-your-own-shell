045b7324283603786c18f06bb2be603f1a617815
DvanXanMalai <xabilal27@gmail.com>
2025-05-29 06:41:26 +0545

    type command

--- main.py
+++ main.py
@@ -6,13 +6,32 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+
+        # exit funciton
         if command == "exit 0":
             sys.exit(0)
-        if command.startswith("echo "):
+
+        # echo function
+        elif command.startswith("echo "):
             echo_text = command.split("echo ")
             if len(echo_text) > 1:
                 print(echo_text[1])
+            else:
+                print("")
+            # sys.stdout.write("$ ")
+
+        # type function
+        elif command.startswith("type "):
+            type_text = command.split("type ")
+            if len(type_text) > 1:
+                if type_text[1] in ("exit", "echo", "type"):
+                    print(type_text[1], "is a shell builtin")
+                else:
+                    print(f"{type_text[1]}: not found")
             # sys.exit(0)
+            else:
+                # Handle "type" with no arguments
+                print("type: usage: type command_name")
         else:
             print(f"{command}: command not found")
 
